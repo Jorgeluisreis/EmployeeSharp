9c43e51e3dfcdfef15800c05896916a410a754bb
Jorgeluisreis <ctt.jorgeluis@gmail.com>
2024-06-14 21:58:07 -0300

    hotfix: correção de ambiente

--- EmployeeSharp.Web/Program.cs
+++ EmployeeSharp.Web/Program.cs
@@ -1,52 +1,50 @@
-using EmployeeSharp.Infra.Data;
+using EmployeeSharp.Application.Services;
 using EmployeeSharp.Domain.Interfaces;
 using EmployeeSharp.Infra.Data.Repositories;
-using EmployeeSharp.Application.Services;
+using EmployeeSharp.Infra.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 var env = builder.Environment;
 var isDevelopment = env.IsDevelopment();
+var isRunningInDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER")?.ToLower() == "true";
 
-if (isDevelopment)
+if (isDevelopment || isRunningInDocker)
 {
-
     builder.Logging.AddConsole();
 }
 
-
 builder.Services.AddRazorPages();
 
-
-var config = new ConfigurationBuilder()
+var configBuilder = new ConfigurationBuilder()
     .SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
     .AddEnvironmentVariables()
-    .AddUserSecrets<Program>()
-    .Build();
+    .AddUserSecrets<Program>();
+
+if (isRunningInDocker)
+{
+    configBuilder.AddEnvironmentVariables(prefix: "DOCKER_");
+}
+
+var config = configBuilder.Build();
 
 var connectionString = config["ConnectionStrings:DefaultConnection"];
+
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new InvalidOperationException("A string de conexão 'DefaultConnection' não foi encontrada.");
 }
 
-
 builder.Services.AddDbContext<EmployeeSharpContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 0))));
-
 
 builder.Services.AddScoped<IColaboradorRepository, ColaboradorRepository>();
 builder.Services.AddScoped<ICargoRepository, CargoRepository>();
 builder.Services.AddScoped<ColaboradorService>();
 builder.Services.AddScoped<CargoService>();
-
 
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
@@ -65,7 +63,6 @@ builder.WebHost.ConfigureKestrel(serverOptions =>
 });
 
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -86,6 +83,5 @@ app.MapControllerRoute(
     pattern: "{controller=Colaboradores}/{action=Index}/{id?}");
 
 app.MapRazorPages();
-
 
 app.Run();
