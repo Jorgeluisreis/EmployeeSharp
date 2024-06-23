3927a05ae606851c1b1a915c837d54abdd45cd71
Jorgeluisreis <ctt.jorgeluis@gmail.com>
2024-06-23 14:23:09 -0300

    refatoração da Program.cs

---
+++ EmployeeSharp.Web/Extensions/KestrelExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeSharp.Web.Extensions
+{
+    public static class KestrelExtensions
+    {
+        public static void ConfigureKestrelServer(this IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder.ConfigureKestrel(serverOptions =>
+            {
+                var env = serverOptions.ApplicationServices.GetRequiredService<IHostEnvironment>();
+
+                if (env.IsDevelopment())
+                {
+                    serverOptions.ListenAnyIP(5000);
+                    serverOptions.ListenAnyIP(5001, listenOptions =>
+                    {
+                        listenOptions.UseHttps();
+                    });
+                }
+                else
+                {
+                    serverOptions.ListenAnyIP(2041);
+                }
+            });
+        }
+    }
+}
